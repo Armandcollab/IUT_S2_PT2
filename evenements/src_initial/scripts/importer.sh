9f97ce50178dd428ccc09164be003d0b898871a9
Lucas Poujardieu <lupoujardieu@info-shelby.iut.bx1>
2020-01-30 10:07:38 +0100

    jalon6 Maj script

--- evenements/src_initial/scripts/importer.sh
+++ evenements/src_initial/scripts/importer.sh
@@ -12,7 +12,10 @@ echo "* Import des salles"
 ./scripts/executer.sh import-salles data/salles/salles.csv
 
 echo "* Import des événements et de leurs séances"
-echo "  A faire"
+./scripts/executer.sh import-seances data/evenements/forum_stages_seances.csv
+./scripts/executer.sh import-seances data/evenements/jpo_seances.csv
+echo "Fait"
+
 
 echo "* Import des séances de l'API"
 echo "  A faire"
