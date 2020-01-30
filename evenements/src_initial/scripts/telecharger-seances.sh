2eb5c19e9e5f18713aab84f78a93e0b001d658bb
Claveau Armand <armand.claveau@etu.u-bordeaux.fr>
2020-01-30 11:41:26 +0100

    jalon5 ajout telechargement-seances.sh

--- evenements/src_initial/scripts/telecharger-seances.sh
+++ evenements/src_initial/scripts/telecharger-seances.sh
@@ -4,4 +4,16 @@
 
 source ./scripts/config.sh
 
-# A implémenter!
+DossierTelCSV=scripts/DossierTelCSV/
+rm $DossierTelCSV/*
+
+* "Lancement des requetes vers l'API"
+for salle in $(./scripts/executer.sh lister-salles)
+do 
+    echo "* Importation des événements des 30 prochains jours de la salle $salle"
+
+    for i in `seq 0 29`
+    do
+        wget -q -P $DossierTelCSV http://info-timide.iut.u-bordeaux.fr/projets/pt2_api/$(date +%Y-%m-%d --date="+$i day")/$salle.csv && mv $DossierTelCSV/$salle.csv $DossierTelCSV/$(date +%Y-%m-%d --date="+$i day")-$salle.csv
+    done
+done
